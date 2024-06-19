cc3df0692cb85321f8a88cb91c1608cb223ae137
TanmayMachkar <tanmaymacho@gmail.com>
2024-06-19 11:21:15 +0530

    libraries and interfaces added

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,20 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.24",
+  solidity: {
+    compilers: [
+      { version: "0.5.5" },
+      { version: "0.6.6" },
+      { version: "0.8.8" },
+    ],
+  },
+  networks: {
+    hardhat: {
+      forking: {
+        url: "https://bsc-dataseed1.binance.org/",
+      },
+    },
+  },
 };
